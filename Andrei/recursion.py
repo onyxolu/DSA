98cb62fc73c024fd1b6bfc8ce6a8d43302cfe4e2
Olumide Ogundare <ogundareolumide@gmail.com>
2020-11-18 10:00:27 +0100

    Cleaned up the code

--- Andrei/recursion.py
+++ Andrei/recursion.py
@@ -17,5 +17,71 @@ def getFactR(num):
 
 print(getFactR(5))
 
+#  0,1,1,2,3,5,8,13,21,34
+
+def fibo(index):
+    if index == 0:
+        return 0
+    pos =  1
+    fib = [0,1]
+    while pos != index:
+        fib.append(fib[pos]+fib[pos-1])
+        pos+=1
+    return fib[index]
+
+print(fibo(10))
+
+def fiboR(index):
+    if index < 2:
+        return index
+    
+    return fiboR(index-1) + fiboR(index-2)
+
+print(fiboR(10))
+
+# function reverseStringRecursive (str) {
+#   if (str === "") {
+#     return "";
+#   } else {
+#     return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+#   }
+# }
+
+def reversestring(string):
+    if string == "":
+        return ""
+    return reversestring(string[1:]) + string[0]
+
+print(reversestring("olumide"))
+     
+
+# Don't use zero as base case
+
+# fib_cache - {}
+
+# optimal solution using memoization
+# def fiboR(index):
+#     if index in fib_cache:
+#         return fib_cache[index]
+#     if index < 2:
+#         return index
+    
+#     value = fiboR(index-1) + fiboR(index-2)
+#     fib_cache[index] = value
+#     return value
 
 
+# Recursion Examples
+# Merge sort, Quick Sort
+# Tree Traversal
+# Graph Traversal
+
+
+
+# Bootle necks
+# Stack Overflow
+# Space Complexity
+
+# Pros
+# Readability
+# sometimes efficiency especially with memoization
