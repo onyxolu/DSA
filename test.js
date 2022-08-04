f420163a36ab091e6b9df0f9c076ba77ecb4e54a
Olumide Ogundare <oogundare@microsoft.com>
2022-08-04 10:20:19 +0100

    added validateEmail check

--- test.js
+++ test.js
@@ -91,6 +91,14 @@ const validateRequired = (required, data) => {
     return "field is required"
 }
 
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 
 
 
